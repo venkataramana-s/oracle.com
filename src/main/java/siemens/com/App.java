e942a96b0693d6eaa560765ced1748dd9aea9485
venkataramana <venkataramanasandhireddy@gmail.com>
2020-01-08 15:25:34 +0530

    first commit

---
+++ src/main/java/siemens/com/App.java
@@ -0,0 +1,13 @@
+package siemens.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
